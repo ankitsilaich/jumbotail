635c257b0c21e01d1c9408c0e7ad5876771008b6
Ankit Kumar <ankit.kumar@opinioapp.com>
2016-08-07 14:02:07 +0530

    adding build system to deploy to ghpages

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf out || exit 0;
+( cd build
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+)
+Contact GitHub API Training Shop Blog About
